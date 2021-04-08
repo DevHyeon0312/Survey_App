ae7f7273978762501a4fa555d38bec56b14908b1
devhyeon <devhyeon0312@gmail.com>
2021-04-08 21:25:07 +0900

    URL

--- app/src/main/java/com/devhyeon/survey/Constants.kt
+++ app/src/main/java/com/devhyeon/survey/Constants.kt
@@ -3,6 +3,6 @@ package com.devhyeon.survey
 /** App 공통 상수 */
 
 const val SPLASH_DELAY = 1000
-const val BASE_URL = "http://10.211.55.11:312/"
+const val BASE_URL = "URL_ADDRESS"
 const val RECIPE_ITEM_KEY = "RECIPE_ITEM_KEY"
 const val SHARED_PREFERENCES_FILE_NAME = "SURVEY_APP"
